3998ea3eeba38ab2bd75af0d48ce2b5aa4857cbc
1815925301 <yuanhuiliang715@163.com>
2018-11-18 22:47:35 +0800

    frist commit

---
+++ yuan-achieve/src/main/java/com/yuan/servlet/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.yuan.servlet;
+
+import com.yuan.utils.DateTimeUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * FileName: HelloServlet
+ * Author:   yhl
+ * Date:     2018/11/17 21:52
+ * Description: ${DESCRIPTION}
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    private static Logger logger = LogManager.getLogger(HelloServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String createTime = DateTimeUtils.getNowTime();
+        req.setAttribute("createTime1",111);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+    }
+}
---
+++ yuan-achieve/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: yhl
+  Date: 2018/11/17
+  Time: 23:51
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8"%>
+<html>
+<head>
+    <title>hello页面</title>
+</head>
+<body>
+<h1>当前时间:</h1>
+<h2>${createTime1}</h2>
+</body>
+</html>
